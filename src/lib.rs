1c320544ca5f6a77345dfd47745541d9e64cb3a5
Sergio Arroutbi <sarroutb@redhat.com>
2022-08-19 13:04:50 +0200

    Test with cargo bin and lib documentation
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! # Cargo Walkthrough
+//!
+//! `cargo_walkthrough` is a simple library to return a message
+
+/// Returns a hello message
+pub fn hello_msg() -> String {
+    "Hello, I am Cargo Walktrhough!".to_string()
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/// Testing cargo documentation
+///
+/// # Example of execution
+///
+/// ```
+/// ./target/debug/cargo_walkthrough
+/// ```
+
+fn main() {
+    println!("{}", cargo_walkthrough::hello_msg());
+}
